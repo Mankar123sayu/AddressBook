777b596c37923043097ec80d5a0c4de5232caa7b
Mankar123sayu <125425650+Mankar123sayu@users.noreply.github.com>
2023-03-15 03:57:15 +0530

    Update Program.cs

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -6,7 +6,14 @@ namespace AddressBook
         public void Main(string[] args)
         {
             AddressBookMain addressbookMain = new AddressBookMain();
+            for (int i = 0; i < 3; i++)
+            {
+                AddressBook.AddContact();
+            }
             addressBook.AddContact();
+            //addressBook.EditContact();
+            //addressBook.Display();
+            addressBook.EditContact();
             addressBook.Display();
         }
     }
